84165dd5920830a35612e73f39ee63d041c831dd
carrotbat410 <carrotbat410@gmail.com>
2024-08-11 00:07:42 +0900

    styles-Color Palette docs 연습

---
+++ src/pages/Colors.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+    <div>
+      <q-btn label="버튼" color="error" text-color="black"></q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.text-error {
+  color: yellow;
+}
+.bg-error {
+  color: black;
+}
+</style>
